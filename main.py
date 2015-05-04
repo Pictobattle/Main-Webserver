2422125a6671688a9402f022b662aa01cdd70bf1
Lucien Gaitskell <lucien@gaitskell.com>
2015-05-04 15:57:09 -0400

    adding main code

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+
+if __name__ == "__main__":
+	app.secret_key = 'Ymsf,sfatwBU!Iwruh,bus'
+	app.debug = True
+	app.run(
+		host='0.0.0.0',
+		port=80
+		)
