008bd08f99ff828a94c9f76290dcdb066ec6a9f8
Lucien Gaitskell <lucien@gaitskell.com>
2015-05-21 17:34:33 -0400

    test

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-	return "<h1>Looking for Picto Battle</h1> <br>Uhmmm... how do I say this... <br> <strong>WE AREN'T DONE DEVELOPING YET!"
+	return "<h1>Looking for Picto Battle</h1>Uhmmm... how do I say this... <br> <strong>WE AREN'T DONE DEVELOPING YET!</strong>"
 
 if __name__ == "__main__":
 	app.secret_key = 'Ymsf,sfatwBU!Iwruh,bus'
