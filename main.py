8cca56cfc14187e6ba8ac44b233ee95d56048480
brookstelfeian <jcamburn@yahoo.com>
2015-05-25 10:31:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def main():
 	return "<head><title>bro</title></head><h1>Looking for Picto Battle</h1>Uhmmm... how do I say this... <br> <strong>WE AREN'T DONE DEVELOPING YET!</strong>"
-
+<head>Picto Battle will be on the app store later<br>untill then<strong>bye bye
 if __name__ == "__main__":
 	app.secret_key = 'Ymsf,sfatwBU!Iwruh,bus'
 	app.debug = True
