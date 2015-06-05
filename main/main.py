b6d8e1154eb3123d96f1dd07a1ef22fc1a1b1dc4
Lucien Gaitskell <lucien@gaitskell.com>
2015-06-04 21:31:37 -0400

    changed secret key!

--- main/main.py
+++ main/main.py
@@ -40,7 +40,7 @@ def favicon():
 	return redirect(url_for('static', filename='favicon.ico'))"""
 
 if __name__ == "__main__":
-	app.secret_key = 'Ymsf,sfatwBU!Iwruh,bus'
+	app.secret_key = 'RC0ZT6kkTQUSHt6hf0GFHhgddWaew3ApGRZnzcPPtClT4zhKiD6OWhlW4t2Z0X2'
 	app.debug = True
 	app.run(
 		host='127.0.0.1',
