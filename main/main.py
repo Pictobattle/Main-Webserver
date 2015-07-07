8843985bbb2ec31e5b3d6bbd44aec8ba35e3d05e
Lucien Gaitskell <lucien@gaitskell.com>
2015-07-07 18:00:30 -0400

    made new main file

---
+++ main/main.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+@app.route('/')
+def main():
+	return render_template('main.html')
+
+#--------------------------HTTP Error Pages:------------------------------------
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('errorPages/404.html'), 404
+
+@app.errorhandler(500)
+def server_error(e):
+	return render_template('errorPages/50x.html'), 500
+
+
+if __name__ == "__main__":
+	app.secret_key = 'RC0ZT6kkTQUSHt6hf0GFHhgddWaew3ApGRZnzcPPtClT4zhKiD6OWhlW4t2Z0X2'
+	app.debug = True
+	app.run(
+		host='127.0.0.1',
+		port=4000
+		)
