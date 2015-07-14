a49d059b40f62867ab1da0d008bb32677dbc8831
Lucien Gaitskell <lucien@gaitskell.com>
2015-07-14 19:35:00 -0400

    got a barebones signup using mysql

--- main/main.py
+++ main/main.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import MySQLdb
 
 app = Flask(__name__)
 
 localTest=True
+
+sqlDuplicateErrorCode = 1062
 
 sqlHost="localhost"
 if localTest:
@@ -18,14 +20,36 @@ db = MySQLdb.connect(host=sqlHost,
 def main():
 	return render_template('main.html')
 
-@app.route('/signUp', methods=['GET','POST'])
+@app.route('/user/signUp', methods=['GET','POST'])
 def signUp():
-	global db # getting global db varible
 
-	cur = db.cursor() # creating SQL cursor
-	cur.execute("SELECT * FROM profiles")
+	if request.method == 'GET':
+		return render_template('userSignUp.html')
+	else:
+		global db # getting global db varible
 
-	return str(cur.fetchall())
+		email=request.form.get("email")
+		email=MySQLdb.escape_string(email)
+		password=request.form.get("password")
+		password=MySQLdb.escape_string(password)
+
+		cur = db.cursor() # creating SQL cursor
+
+		try:
+			cur.execute('INSERT INTO logins (email,password) VALUES ("' + email + '","' + password + '");')
+		except MySQLdb.IntegrityError, message:
+			errorcode = message[0]
+			if errorcode == sqlDuplicateErrorCode:
+				return "a dupe"
+			else:
+				raise # unexpected error, reraise
+
+		cur.execute("SELECT * FROM logins")
+
+		tableVals=[]
+		for ii in cur.fetchall():
+			tableVals.append(ii)
+		return str(tableVals)
 
 @app.route('/areWeOnline')
 def thisIsStatic():
