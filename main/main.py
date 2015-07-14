a77efdf43bc96df3f018dd57090828aee30e00b5
Lucien Gaitskell <lucien@gaitskell.com>
2015-07-13 21:31:54 -0400

    added test sql stuff

--- main/main.py
+++ main/main.py
@@ -3,14 +3,29 @@ import MySQLdb
 
 app = Flask(__name__)
 
+localTest=True
+
+sqlHost="localhost"
+if localTest:
+	sqlHost="lucieng.ddns.net"
+
+db = MySQLdb.connect(host=sqlHost,
+                     user="pictoext",
+                      passwd="picto1Battle",
+                      db="pictobattle")
+
 @app.route('/')
 def main():
 	return render_template('main.html')
 
 @app.route('/signUp', methods=['GET','POST'])
 def signUp():
+	global db # getting global db varible
 
-	return "yo"
+	cur = db.cursor() # creating SQL cursor
+	cur.execute("SELECT * FROM profiles")
+
+	return str(cur.fetchall())
 
 @app.route('/areWeOnline')
 def thisIsStatic():
