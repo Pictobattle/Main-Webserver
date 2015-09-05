a1e517fef95ee52937805c9078b8fe213d041411
Lucien Gaitskell <lucien@gaitskell.com>
2015-09-04 20:55:19 -0400

    return main

--- main/main.py
+++ main/main.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return render_template('main.html')
+
 if __name__ == "__main__":
 	app.secret_key = 'RC0ZT6kkTQUSHt6hf0GFHhgddWaew3ApGRZnzcPPtClT4zhKiD6OWhlW4t2Z0X2'
 	app.debug = True
