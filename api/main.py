c5d6c9fd615ee29a3fc3a5d8de854e596a4a470e
Lucien Gaitskell <lucien@gaitskell.com>
2015-09-04 20:55:29 -0400

    changed sql creds

--- api/main.py
+++ api/main.py
@@ -8,12 +8,13 @@ localTest=True
 sqlDuplicateErrorCode = 1062
 
 sqlHost="127.0.0.1"
-sqlPass="pictobattle"
+sqlPass=""
+sqlUser="pictobattle"
 sqlDb="pictobattle"
 if localTest:
+	sqlHost="lucieng.ddns.net"
 	sqlPass="picto1Battle"
 	sqlUser="pictoext"
-	sqlHost="lucieng.ddns.net"
 
 '''readDb = MySQLdb.connect(
 	host="lucieng.ddns.net",
