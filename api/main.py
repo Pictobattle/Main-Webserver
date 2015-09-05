0d6021791eacb41d83df2002b7ccdff19df663e2
Lucien Gaitskell <lucien@gaitskell.com>
2015-09-04 20:25:20 -0400

    removed api secret key

--- api/main.py
+++ api/main.py
@@ -181,7 +181,6 @@ def server_error(e):
 
 if __name__ == "__main__":
 	readDb=sqlServerConnect()
-	app.secret_key = 'RC0ZT6kkTQUSHt6hf0GFHhgddWaew3ApGRZnzcPPtClT4zhKiD6OWhlW4t2Z0X2'
 	app.debug = True
 	app.run(
 		host='127.0.0.1',
