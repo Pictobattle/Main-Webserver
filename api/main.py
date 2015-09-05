16933ddf471e90799bc9e17f7adc00d9c1962e73
Lucien Gaitskell <lucien@gaitskell.com>
2015-09-04 20:55:08 -0400

    changed port

--- api/main.py
+++ api/main.py
@@ -184,5 +184,5 @@ if __name__ == "__main__":
 	app.debug = True
 	app.run(
 		host='127.0.0.1',
-		port=4000
+		port=8081
 		)
--- main/main.py
+++ main/main.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
 	app.debug = True
 	app.run(
 		host='127.0.0.1',
-		port=4000
+		port=8080
 		)
